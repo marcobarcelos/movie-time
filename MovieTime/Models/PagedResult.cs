296bf7667f0677392355ef00ccc0368bf350c27e
Marco Barcelos <me@marcobarcelos.com>
2019-03-30 17:45:27 -0300

    Add models

---
+++ MovieTime/Models/PagedResult.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace MovieTime.Models
+{
+    public class PagedResult<T>
+    {
+        [JsonProperty(PropertyName = "page")]
+        public int Page { get; set; }
+
+        [JsonProperty(PropertyName = "results")]
+        public List<T> Results { get; set; }
+
+        [JsonProperty(PropertyName = "total_pages")]
+        public int TotalPages { get; set; }
+
+        [JsonProperty(PropertyName = "total_results")]
+        public int TotalResults { get; set; }
+    }
+}
